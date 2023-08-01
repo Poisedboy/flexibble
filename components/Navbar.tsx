7a879b8997bf88854fba2a20d3fcdbcf374c5d56
Serhii <beschasnuy96@gmail.com>
2023-08-01 17:40:08 +0300

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { NavLinks } from '@/constants'
+import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+import { signOut } from 'next-auth/react'
+import ProfileMenu from './ProfileMenu'
+
+const Navbar = async () => {
+    const session = await getCurrentUser()
+
+    return (
+        <nav className='flexBetween navbar'>
+            <div className='flex-1 flexStart gap-10'>
+                <Link href='/'>
+                    <Image
+                        src='/logo.svg'
+                        alt='Flexibble'
+                        width={115}
+                        height={43}
+                        style={{ width: 115, height: 43 }}
+                    />
+                </Link>
+                <ul className='xl:flex hidden text-small gap-7'>
+                    {NavLinks.map(link => (
+                        <Link
+                            href={link.href}
+                            key={link.key}
+                        >
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className='flexCenter gap-4'>
+                {session?.user ? (
+                    <>
+                        <ProfileMenu session={session} />
+                        {/* {session?.user?.image && (
+                            <Link href={`/profile/${session?.user?.id}`}>
+                                <Image
+                                    src={session.user.image}
+                                    alt='logo'
+                                    width={40}
+                                    height={40}
+                                    className='rounded-full'
+                                />
+                            </Link>
+
+                        )} */}
+                        <Link href='/create-project'>Share Work</Link>
+                    </>
+                ) : (
+                    <AuthProviders />
+                )}
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
